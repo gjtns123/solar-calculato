390302d3720257404976f906dfd4913102df6c12
gjtns123 <57667947+gjtns123@users.noreply.github.com>
2026-04-13 14:45:21 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'solar-calc-v2';
+const ASSETS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)).catch(() => caches.match('./index.html')));
+});
